f87a8d435f314e420370d3f99dbe45d20a53e512
Eeva-Jonna <eevajonna.panula@gmail.com>
2018-10-24 14:43:47 +0300

    add Google Analytics

--- site-config.js
+++ site-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'SRD Gameday info',
   description: 'Site for SRD game day',
   // Services:
-  googleAnalyticsTrackingId: `ADD YOUR TRACKING ID HERE`,
+  googleAnalyticsTrackingId: `UA-123728910-2`,
 
   // Site Style:
   theme: 'grommet', // dark | hpe | grommet
